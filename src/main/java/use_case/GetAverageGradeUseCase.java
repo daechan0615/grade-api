27f27224ebaae936650038eaf296e9813b518d95
daechan eom <d.eom@mail.utoronto.ca>
2023-09-25 20:33:04 -0400

    Final

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -12,6 +12,19 @@ public final class GetAverageGradeUseCase {
 
     public float getAverageGrade(String course) {
         // TODO: Get average grade for all students in your team.
-        return 0.0f;
+//        String[] members = gradeDB.getMyTeam().getMembers();
+//        float grades = 0.0f;
+//        for (String member : members) {
+//            grades += gradeDB.getGrade(member, course).getGrade();
+//        }
+//        return grades / members.length;
+        float total = 0;
+        Team team = gradeDB.getMyTeam();
+        for (String s: team.getMembers()) {
+            Grade g = gradeDB.getGrade(s, course);
+
+            total += g.getGrade();
+        }
+        return total / team.getMembers().length;
     }
 }
